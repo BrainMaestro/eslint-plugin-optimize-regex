851a060526ed04fdb06d0eaed27e6e0525905a9a
Ezinwa Okpoechi <brainmaestro@outlook.com>
2017-04-14 23:28:18 +0100

    Add plugin functionality

---
+++ tests/lib/rules/optimize-regex.js
@@ -0,0 +1,31 @@
+/**
+ * @fileoverview Optimize regex literals
+ * @author Ezinwa Okpoechi
+ */
+
+'use strict';
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require('../../../lib/rules/optimize-regex'),
+  RuleTester = require('eslint').RuleTester;
+
+var ruleTester = new RuleTester();
+ruleTester.run('optimize-regex', rule, {
+  valid: ['var foo = /foo/i'],
+
+  invalid: [
+    {
+      code: 'var re = /[a-zA-Z_0-9][A-Z_\\da-z]*\\e{1,}/',
+      output: 'var re = /\\w+e+/',
+      errors: [
+        {
+          message: '/[a-zA-Z_0-9][A-Z_\\da-z]*\\e{1,}/ can be optimized to /\\w+e+/',
+          type: 'Literal',
+        },
+      ],
+    },
+  ],
+});
